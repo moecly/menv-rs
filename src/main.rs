37c2ae2906d7158f037fb1ee72ef1b6484495c5d
moecly <1349269087ABC@gmail.com>
2026-06-04 08:39:52 +0800

    feat: 添加日志和异步库

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::eyre::{Ok, Result};
+use tracing::info;
+use tracing_subscriber::{EnvFilter, fmt};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+    fmt()
+        .with_env_filter(EnvFilter::try_from_default_env().unwrap_or("info".into()))
+        .with_file(true)
+        .with_line_number(true)
+        .init();
+    info!("helloworld");
+    Ok(())
 }
