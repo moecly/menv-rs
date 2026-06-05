6bc01189b98c84e945046b8bf18f123a28303948
moecly <1349269087ABC@gmail.com>
2026-06-05 17:32:36 +0800

    feat: 实现link

--- src/cli/repos.rs
+++ src/cli/repos.rs
@@ -30,7 +30,9 @@ impl Repos {
             ReposCommands::Pull => {
                 Self::pull_all(cfg).await?;
             }
-            ReposCommands::Link => {}
+            ReposCommands::Link => {
+                Self::link_all(cfg).await?;
+            }
         }
         Ok(())
     }
@@ -160,5 +162,63 @@ impl Repos {
         }
 
         Ok(str::from_utf8(&output.stderr)?.to_string())
+    }
+
+    async fn link_all(cfg: &CliConfig) -> Result<()> {
+        let repos = cfg.get_repos_config();
+        let success = Arc::new(Mutex::new(0));
+        let failed = Arc::new(Mutex::new(0));
+        let total = repos.len();
+        let mut handles = Vec::new();
+        Common::print_emoji_title("🔗", "Repository Link");
+        repos.iter().for_each(|r| {
+            let tmpr = RepoConfig {
+                name: r.name.clone(),
+                git_url: r.git_url.clone(),
+            };
+            let success = Arc::clone(&success);
+            let failed = Arc::clone(&failed);
+            handles.push(spawn_blocking(move || match Self::link(&tmpr) {
+                Ok(_) => {
+                    Common::print_progress_done(&tmpr.name);
+                    *success.lock() += 1;
+                }
+                Err(e) => {
+                    Common::print_progress_failed(&tmpr.name, &e.to_string());
+                    *failed.lock() += 1;
+                }
+            }));
+        });
+
+        for handle in handles {
+            handle.await?;
+        }
+
+        Common::print_summary(*success.lock(), *failed.lock(), total);
+        Ok(())
+    }
+
+    fn link(repo: &RepoConfig) -> Result<()> {
+        let work_dir = Common::get_cfg_path()?.join(&repo.name);
+        if !work_dir.exists() {
+            bail!(
+                "repo: {} not exists",
+                work_dir.to_str().context("pathbuf to str failed")?
+            );
+        }
+
+        let output = Command::new("sh")
+            .arg("link.sh")
+            .current_dir(work_dir)
+            .output()?;
+
+        if !output.status.success() {
+            bail!(
+                "failed to git pull: {}, {}",
+                repo.name,
+                str::from_utf8(&output.stderr)?
+            );
+        }
+        Ok(())
     }
 }
